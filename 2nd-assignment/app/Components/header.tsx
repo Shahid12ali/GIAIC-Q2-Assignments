88bb3d1e396c2b5b54ae3274a0371b7558a058cb
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-09-22 18:33:47 +0500

    Created 2 Components

---
+++ 2nd-assignment/app/Components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Wellcome To Next.Js</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+};
---
+++ 2nd-assignment/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./Components/header";
+import About from "./Components/about";
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <About/>
+    </div>
+  );
+}
